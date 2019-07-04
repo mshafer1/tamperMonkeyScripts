c5f11e49cba429ecdfe86debc7785311ef5c5da0
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-07-04 09:20:38 -0500

    make case insensitive and add license

--- Facebook Political Blocker.user.js
+++ Facebook Political Blocker.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Facebook Political Blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Disable posts on Facebook that contain any of the keywords in the list below. Posts can be re-enabled by clicking on them.
-// @author       You
+// @author       mshafer1 (github.com/mshafer1)
 // @match        https://www.facebook.com/*
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @licence      GPL-3.0-or-later; https://github.com/mshafer1/tamperMonkeyScripts/blob/master/LICENSE
+// @copyright    2019+, mshafer1
 // ==/UserScript==
 
 (function() {
@@ -14,7 +16,7 @@
     var keywords = ['politics', 'Trump', 'election', 'political', 'vote', 'voting'];
 
     function check(word, html) {
-        return html.search(word) != -1;
+        return html.toLowerCase().search(word.toLowerCase()) != -1;
     }
 
     function fadeIn() {
