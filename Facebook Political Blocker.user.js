eb8421a5a4ea847b86fb21d06b1875cf1dab3b73
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-07-07 16:16:27 -0500

    moving core functionality to include script for independent updating

--- Facebook Political Blocker.user.js
+++ Facebook Political Blocker.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Facebook Political Blocker
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Disable posts on Facebook that contain any of the keywords in the list below. Posts can be re-enabled by clicking on them.
 // @author       mshafer1 (github.com/mshafer1)
+// @include      facebook_blocker_includes/methods.js
 // @match        https://www.facebook.com/*
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // @licence      GPL-3.0-or-later; https://github.com/mshafer1/tamperMonkeyScripts/blob/master/LICENSE
@@ -14,29 +15,6 @@
     'use strict';
 
     var keywords = ['politics', 'Trump', 'election', 'political', 'vote', 'voting'];
-
-    function check(word, html) {
-        return html.toLowerCase().search(word.toLowerCase()) != -1;
-    }
-
-    function fadeIn() {
-        $(this).attr('disabled', false);
-        $(this).fadeTo('slow', 1);
-        $(this).off();
-    }
-
-    function checkScreen() {
-        $('.userContentWrapper').each(function () {
-            // console.log($(this));
-            var html = $(this).first().html();
-            if(keywords.some(function (e) {return check(e, html);})) {
-                // console.log("Match");
-                $(this).attr('disabled', true);
-                $(this).fadeTo('fast', .1);
-                $(this).click(fadeIn);
-            }
-        });
-    }
 
     $(window).scroll(checkScreen);
 })();
---
+++ facebook_blocker_includes/methods.js
@@ -0,0 +1,22 @@
+function check(word, html) {
+    return html.toLowerCase().search(word.toLowerCase()) != -1;
+}
+
+function fadeIn() {
+    $(this).attr('disabled', false);
+    $(this).fadeTo('slow', 1);
+    $(this).off();
+}
+
+function checkScreen() {
+    $('.userContentWrapper').each(function () {
+        // console.log($(this));
+        var html = $(this).first().html();
+        if(keywords.some(function (e) {return check(e, html);})) {
+            // console.log("Match");
+            $(this).attr('disabled', true);
+            $(this).fadeTo('fast', .1);
+            $(this).click(fadeIn);
+        }
+    });
+}
